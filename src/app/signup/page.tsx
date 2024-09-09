779cab7f53ddb6a0c57b148fcb4de6d5e1098f79
Sayan Das <sayandasraja56@gmail.com>
2024-09-09 13:37:43 +0530

    setup

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+    return (
+        <div className="flex flex-row justify-center items-center ">
+        <h1 className="text-center ">Signup</h1>
+        </div>
+    )
+}
