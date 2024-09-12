6b9c432c42911de101b24f25469cce1f8a0818a2
Sayan Das <sayandasraja56@gmail.com>
2024-09-13 01:57:27 +0530

    full signup & login

--- src/dbconfig/dbConfig.ts
+++ src/dbconfig/dbConfig.ts
@@ -1,19 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGODB_URI!)
+        mongoose.connect(process.env.MONGODB_URI!);
         const connection = mongoose.connection;
+
         connection.on('connected', () => {
-            console.log("Connected to MongoDB");
-        }
-        )
-        connection.on('error', (error) => {
-            console.log("Error: ", error);
-            process.exit(1);
-        }
-        )
+            console.log('MongoDB connected successfully');
+        })
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
+        })
+
     } catch (error) {
-        console.log("Error: ", error);
+        console.log('Something goes wrong!');
+        console.log(error);
+        
     }
+
+
 }
